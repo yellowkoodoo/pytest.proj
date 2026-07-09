95c07233d8e85e2317a00abe8277d185764a5a1a
yevhenii kotikov <yevheniikotikov@gmail.com>
2026-07-09 15:17:59 +0300

    app fixture

--- framework/pages/app.py
+++ framework/pages/app.py
@@ -3,11 +3,19 @@ from functools import cached_property
 from playwright.sync_api import Page
 
 from framework.pages.shop_pages.login_page import LoginPage
+from framework.pages.shop_pages.top_bar_panel import TopBarPanel
 
 
 class App:
     def __init__(self, page: Page):
         self.page = page
+
+    def open(self):
+        self.page.goto("/")
+
+    @cached_property
+    def top_bar(self):
+        return TopBarPanel(self.page)
 
     # @property 
     @cached_property
